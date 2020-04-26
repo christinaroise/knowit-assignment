a9275c8051ef85c8f70333f1fc8dccccf9d34807
christinaroise <54266462+christinaroise@users.noreply.github.com>
2020-04-26 21:09:01 +0200

    Updates

---
+++ precache-manifest.95e3e8cbe1d55756af63be353d888427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e715d53fe9265ce41337e9fe0b5184f0",
+    "url": "/knowit-assignment/index.html"
+  },
+  {
+    "revision": "052af2bb4b6d8e0c6516",
+    "url": "/knowit-assignment/static/css/main.8696375e.chunk.css"
+  },
+  {
+    "revision": "7b5fff0f448c91250ff4",
+    "url": "/knowit-assignment/static/js/2.6f4531d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/knowit-assignment/static/js/2.6f4531d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052af2bb4b6d8e0c6516",
+    "url": "/knowit-assignment/static/js/main.966769ee.chunk.js"
+  },
+  {
+    "revision": "3f709cff0751684b7b48",
+    "url": "/knowit-assignment/static/js/runtime-main.bf4f09ee.js"
+  }
+]);
